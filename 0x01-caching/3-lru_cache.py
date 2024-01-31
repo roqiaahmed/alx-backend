1c0d27c2edfbe630a64f155cea50e6b2ba15f1a1
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2024-01-31 22:19:19 +0200

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
 """a class LRUCache that inherits from BaseCaching and is a caching system"""
+
 from collections import OrderedDict
 
 BaseCaching = __import__("base_caching").BaseCaching

865309d56dc6ec19066a704bc596d060dc3d0ffc
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-01-31 15:02:23 +0200

    lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -19,11 +19,9 @@ class LRUCache(BaseCaching):
         if key is not None and item is not None:
             if key in self.cache_data:
                 self.cache_data.pop(key)
-
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 first = self.cache_data.popitem(last=False)[0]
                 print("DISCARD: {}".format(first))
-
             self.cache_data[key] = item
             self.cache_data.move_to_end(key)
 
@@ -31,7 +29,6 @@ class LRUCache(BaseCaching):
         """get item"""
         if key is None or key not in self.cache_data:
             return None
-
         value = self.cache_data[key]
         self.cache_data.move_to_end(key)
         return value

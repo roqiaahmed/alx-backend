5f65b862ff5a64ca3458d4d858e9d0661ea9414a
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-06 17:23:18 +0200

    Get locale from request

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+"""
+1. Basic Babel setup
+"""
 from flask import Flask, render_template
 from flask_babel import Babel
-
-app = Flask(__name__)
 
 
 class Config:
@@ -12,6 +14,7 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 

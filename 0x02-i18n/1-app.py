07b9ed2a1f88cb9bdb0243059e9ae00829420329
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-06 16:25:30 +0200

    flask_babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -19,7 +19,7 @@ babel = Babel(app)
 @app.route("/")
 def index():
     """default route"""
-    return render_template("0-index.html")
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":

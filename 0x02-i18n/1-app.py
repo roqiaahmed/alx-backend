636528b59fe247e33609d59ffd6017c4a1d0de29
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-06 16:22:18 +0200

    queens

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,16 +4,22 @@ from flask_babel import Babel
 app = Flask(__name__)
 
 
-class Config(object):
+class Config:
+    """Config class"""
+
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-babel = Babel(app, default_locale="en", default_timezone="UTC")
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route("/")
 def index():
-    return render_template("1-index.html")
+    """default route"""
+    return render_template("0-index.html")
 
 
 if __name__ == "__main__":

693b624392d4446149d87f50a530af703ce27a8b
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-06 18:37:17 +0200

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+3. Parametrize templates
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """
+    Config
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """get_locale"""
+    return request.accept_languages(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index():
+    """index"""
+    return render_template(
+        "3-index.html",
+        home_title=_("Welcome to Holberton"),
+        home_header=_("Hello world!"),
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
